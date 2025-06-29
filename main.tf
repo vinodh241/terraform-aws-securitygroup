ed691d66b7c85485a9255c7a620270278b1126ba
vinodh241 <a.vinodh9072@gmail.com>
2025-06-29 15:10:06 +0530

    terraform-aws-security-group

--- main.tf
+++ main.tf
@@ -5,7 +5,14 @@ resource "aws_security_group" "main" {
   name        = var.sg_name
   description = var.sg_description
   vpc_id      = var.vpc_id
-  
+   
+     egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+     }
   tags = merge(
       var.sg_tags,
    local.common_tags,
--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
 output "sg_id" {
   value = aws_security_group.main.id
 }
+
+output "subnet_id" {
+  value = aws_subnet.public[*].id
+  
+}
