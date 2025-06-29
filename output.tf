7fa515356c474c80ad6dfa84e6f5cdfa1e261f88
vinodh241 <a.vinodh9072@gmail.com>
2025-06-29 16:49:54 +0530

    terraform-aws-security-group

--- output.tf
+++ output.tf
@@ -2,8 +2,8 @@ output "sg_id" {
   value = aws_security_group.main.id
 }
 
-output "subnet_id" {
-  value = aws_subnet.public[*].id
+# output "subnet_id" {
+#   value = aws_subnet.public[*].ids
   
 
-}
+# }
